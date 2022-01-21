b09a2abb64330a51ea55a03d3e59722af5e814d6
Marija <macenceee@yahoo.com>
2022-01-21 16:08:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <h1>Marija's Info Site</h1>
+         <ul>
+         <li>      <a href="http://google.com"> </a> Link </li>
+         </ul>
+    </div>
+  );
+}
+
+export default App;
