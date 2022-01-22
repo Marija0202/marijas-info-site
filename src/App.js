3dc20c19173179a3880382f52f67dcb06bc64b4f
Marija <macenceee@yahoo.com>
2022-01-22 11:32:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-         <h1>Marija's Info Site       probe  </h1>
+         <h1>Marija's Info Site</h1>
          <ul>
          <li>      <a href="http://google.com"> </a> Link </li>
          
          </ul>
-    </div>
+    </div> 
   );
 }
 
